90f86948853a8abe00aa86307c346684d5db1eb4
Muhammad Hamza <hamza.6.shakeel@gmail.com>
2023-02-04 19:08:57 +0500

    wip

---
+++ scripts.js
@@ -0,0 +1,33 @@
+// $(document).ready(
+//     function () {
+//         var projects = [
+//             {
+//                 "title": "The Holy Qur'an",
+//                 "labels": {
+//                     "a_store": true,
+//                     "g_store": true,
+//                     "web": true,
+//                     "open_source": true,
+//                     "package": true,
+//                 },
+//             },
+//             {
+//                 "title": "Devfolio",
+//                 "labels": {
+//                     "a_store": false,
+//                     "g_store": true,
+//                     "web": true,
+//                     "open_source": true,
+//                     "package": false,
+//                 },
+//             },
+//         ];
+
+//         var projectsDiv = document.getElementById("projects-id");
+//         var card = document.getElementById("card");
+
+//         for (var i = 0; i < projects.length; i++) {
+//             projectsDiv.appendChild(card);
+//         }
+//     }
+// );
---
+++ scripts/projects.js
@@ -0,0 +1,31 @@
+// var projects = [
+//     {
+//         "title": "The Holy Qur'an",
+//         "labels": {
+//             "a_store": true,
+//             "g_store": true,
+//             "web": true,
+//             "open_source": true,
+//             "package": true,
+//         },
+//     },
+//     {
+//         "title": "Devfolio",
+//         "labels": {
+//             "a_store": false,
+//             "g_store": true,
+//             "web": true,
+//             "open_source": true,
+//             "package": false,
+//         },
+//     },
+// ];
+
+// var projectsDiv = document.getElementById("proejcts-id");
+// var card = document.createElement("div");
+// card.style.height = "100px";
+// card.style.width = "100px";
+
+// for (var i = 0; i < projects.length; i++) {
+//     projectsDiv.appendChild(card);
+// }
