35ba2f4acac39663377e7966ea90d1caec8ef1f9
Muhammad Hamza <hamza.6.shakeel@gmail.com>
2023-02-05 22:07:58 +0500

    some css for UX

--- js/animate.js
+++ js/animate.js
@@ -5,4 +5,27 @@ $(document).ready(function () {
         duration: 1200,
         once: true
     });
+
+    $(document).on("scroll", onScroll);
+
+    //smooth scroll
+    $('a[href^="#"]').on('click', function (e) {
+        e.preventDefault();
+        $(document).off("scroll");
+
+        $('a').each(function () {
+            $(this).removeClass('active');
+        })
+        $(this).addClass('active');
+
+        var target = this.hash,
+            menu = target;
+        $target = $(target);
+        $('html, body').stop().animate({
+            'scrollTop': $target.offset().top + 2
+        }, 1000, 'swing', function () {
+            window.location.hash = target;
+            $(document).on("scroll", onScroll);
+        });
+    });
 });
