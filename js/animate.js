938c29fe97632af3549a7cfb2cde1728856e0326
Muhammad Hamza <hamza.6.shakeel@gmail.com>
2023-02-05 23:02:59 +0500

    calendar added (static)

--- js/animate.js
+++ js/animate.js
@@ -5,27 +5,32 @@ $(document).ready(function () {
         duration: 1200,
         once: true
     });
-
-    $(document).on("scroll", onScroll);
-
-    //smooth scroll
-    $('a[href^="#"]').on('click', function (e) {
-        e.preventDefault();
-        $(document).off("scroll");
-
-        $('a').each(function () {
-            $(this).removeClass('active');
-        })
-        $(this).addClass('active');
-
-        var target = this.hash,
-            menu = target;
-        $target = $(target);
-        $('html, body').stop().animate({
-            'scrollTop': $target.offset().top + 2
-        }, 1000, 'swing', function () {
-            window.location.hash = target;
-            $(document).on("scroll", onScroll);
-        });
-    });
 });
+
+// $(document).ready(function () {
+
+//     window.onscroll = function () {
+//         $(document).on("scroll", onScroll);
+
+//         //smooth scroll
+//         $('a[href^="#"]').on('click', function (e) {
+//             e.preventDefault();
+//             $(document).off("scroll");
+
+//             $('a').each(function () {
+//                 $(this).removeClass('active');
+//             })
+//             $(this).addClass('active');
+
+//             var target = this.hash,
+//                 menu = target;
+//             $target = $(target);
+//             $('html, body').stop().animate({
+//                 'scrollTop': $target.offset().top + 2
+//             }, 1000, 'swing', function () {
+//                 window.location.hash = target;
+//                 $(document).on("scroll", onScroll);
+//             });
+//         });
+//     }
+// });
