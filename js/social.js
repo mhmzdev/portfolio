ebeb7b11041b61bf4074e88972b33f78f4a19d23
Muhammad Hamza <hamza.6.shakeel@gmail.com>
2023-02-06 16:55:25 +0500

    js added for social icons and tech stack

--- js/social.js
+++ js/social.js
@@ -1,6 +1,95 @@
-var data = require('../data/data.json');
-const social = data['social'];
+$(document).ready(function () {
+    var socials = [
+        {
+            "icon": "fa fa-linkedin-square",
+            "value": "https://linkedin.com/in/mhmzdev"
+        },
+        {
+            "icon": "fa fa-github",
+            "value": "https://github.com/mhmzdev"
+        },
+        {
+            "icon": "fa fa-medium",
+            "value": "https://mhmzdev.medium.com"
+        },
+        {
+            "icon": "fa fa-facebook-square",
+            "value": "https://facebook.com/mhmzdev"
+        },
+        {
+            "icon": "fa fa-instagram",
+            "value": "https://instagram.com/mhmzdev"
+        },
+        {
+            "icon": "fa fa-twitter",
+            "value": "https://twitter.com/mhmzdev"
+        }
+    ];
 
-for (var i = 0; i < social.length; i++) {
-    console.log(social[i]["value"]);
-}
+
+    for (var i = 0; i < socials.length; i++) {
+        var div = document.createElement("div");
+        div.className = "social-icon-wrapper";
+
+        div.style.marginLeft = "1%";
+        div.style.padding = "0.75%";
+        div.style.fontSize = "18px";
+        div.style.borderRadius = "50%";
+        div.style.backgroundColor = "#25262A";
+        div.style.display = "flex";
+        div.style.justifyContent = "center";
+        div.style.alignItems = "center";
+        div.style.animation = "beat 1s";
+
+        div.onmouseover = function (event) {
+            div.style.fontSize = "24px";
+            div.style.transition = "0.4s";
+        }
+
+        div.onmouseout = function (event) {
+            div.style.fontSize = "18px";
+            div.style.transition = "0.4s";
+        }
+
+        var icon = document.createElement("i");
+        icon.className = "social-icon " + socials[i]["icon"];
+
+        icon.style.backgroundColor = "#ffffff00";
+        icon.style.color = "#6EF3A5";
+
+        div.appendChild(icon);
+
+        var socialHandles = document.getElementById("social-handles");
+        socialHandles.appendChild(div);
+    }
+
+    for (var i = 0; i < socials.length; i++) {
+        var div = document.createElement("div");
+        div.className = "social-icon-wrapper";
+
+        div.style.marginLeft = "1%";
+        div.style.padding = "2%";
+        div.style.fontSize = "18px";
+        div.style.borderRadius = "50%";
+        div.style.backgroundColor = "#25262A";
+        div.style.display = "flex";
+        div.style.justifyContent = "center";
+        div.style.alignItems = "center";
+        div.style.animation = "beat 1s";
+
+        var icon = document.createElement("i");
+        icon.className = "social-icon " + socials[i]["icon"];
+
+        icon.style.backgroundColor = "#ffffff00";
+        icon.style.color = "#6EF3A5";
+
+        div.appendChild(icon);
+
+        var socialHandlsContact = document.getElementById("social-contact-icons");
+        var extraDiv = document.createElement("div");
+
+        socialHandlsContact.appendChild(extraDiv);
+        socialHandlsContact.appendChild(div);
+        socialHandlsContact.appendChild(extraDiv);
+    }
+});
--- js/tech.js
+++ js/tech.js
@@ -1,27 +1,142 @@
-var data = require('../data/data.json');
-const tech = data['tech'];
+$(document).ready(function () {
+    const tech = [
+        {
+            "name": "Flutter",
+            "icon": "images/tech/flutter.svg",
+            "type": "mobile-tech"
+        },
+        {
+            "name": "Dart",
+            "icon": "images/tech/dart.svg",
+            "type": "mobile-tech"
+        },
+        {
+            "name": "HTML 5",
+            "icon": "images/tech/html.svg",
+            "type": "web-tech"
+        },
+        {
+            "name": "CSS 3",
+            "icon": "images/tech/css.svg",
+            "type": "web-tech"
+        },
+        {
+            "name": "Bootstrap",
+            "icon": "images/tech/bootstrap.svg",
+            "type": "web-tech"
+        },
+        {
+            "name": "Javascript",
+            "icon": "images/tech/js.svg",
+            "type": "web-tech"
+        },
+        {
+            "name": "Flask Restful",
+            "icon": "images/tech/flask.svg",
+            "type": "server-tech"
+        },
+        {
+            "name": "Node.js",
+            "icon": "images/tech/node.svg",
+            "type": "server-tech"
+        },
+        {
+            "name": "Express.js",
+            "icon": "images/tech/express.svg",
+            "type": "server-tech"
+        },
+        {
+            "name": "REST Apis",
+            "icon": "images/tech/api.svg",
+            "type": "server-tech"
+        },
+        {
+            "name": "Firebase",
+            "icon": "images/tech/firebase.svg",
+            "type": "database-tech"
+        },
+        {
+            "name": "MongoDB",
+            "icon": "images/tech/mongo.svg",
+            "type": "database-tech"
+        },
+        {
+            "name": "SQL",
+            "icon": "images/tech/sql.svg",
+            "type": "database-tech"
+        },
+        {
+            "name": "Git & GitHub",
+            "icon": "images/tech/github.svg",
+            "type": "vc-tech"
+        },
+        {
+            "name": "Jira",
+            "icon": "images/tech/jira.svg",
+            "type": "vc-tech"
+        },
+        {
+            "name": "Trello",
+            "icon": "images/tech/trello.svg",
+            "type": "vc-tech"
+        },
+        {
+            "name": "Notion",
+            "icon": "images/tech/notion.svg",
+            "type": "vc-tech"
+        },
+        {
+            "name": "Figma",
+            "icon": "images/tech/figma.svg",
+            "type": "design-tech"
+        },
+        {
+            "name": "Adobe XD",
+            "icon": "images/tech/adobexd.svg",
+            "type": "design-tech"
+        }
+    ];
 
-// div
-var techDiv = document.createElement("div");
-techDiv.className = "tech";
+    for (var i = 0; i < tech.length; i++) {
+        // div
+        var techDiv = document.createElement("div");
+        techDiv.className = "tech";
+        techDiv.style.marginBottom = "15px";
+        techDiv.style.maxWidth = "max-content";
+        techDiv.style.marginRight = "15px";
+        if (tech[i]["name"] == "Node.js") {
+            techDiv.style.paddingLeft = "5px";
+            techDiv.style.paddingRight = "5px";
+        } else {
+            techDiv.style.padding = "5px";
+        }
+        techDiv.style.borderRadius = "6px";
+        techDiv.style.backgroundColor = "#6EF3A5";
 
-// icon
-var techIconImg = document.createElement("img");
-techIconImg.className = "tech-icon";
+        // icon
+        var techIconImg = document.createElement("img");
+        techIconImg.className = "tech-icon";
+        techIconImg.style.backgroundColor = "#ffffff00";
 
-// tech name
-var techName = document.createElement("span");
-techName.className = "tech-label";
+        // tech name
+        var techName = document.createElement("span");
+        techName.className = "tech-label";
+        techName.style.color = "black";
+        techName.style.backgroundColor = "#ffffff00";
+        techName.style.fontSize = "14px";
+        techName.style.paddingRight = "5px";
 
-for (var i = 0; i < tech.length; i++) {
-    // put items in the above div
-    techIconImg.src = tech[i]["icon"];
-    techIconImg.alt = tech[i]["name"];
+        techIconImg.src = tech[i]["icon"];
+        techIconImg.alt = tech[i]["name"];
 
-    console.log(tech[i]["name"]);
-}
+        techName.innerHTML = tech[i]["name"];
 
-// render the div's in specific type e.g. web or mobile etc.
-// id="web-tech"
+        var relatedDiv = document.getElementById(tech[i]["type"]);
+        relatedDiv.style.display = "flex";
+        relatedDiv.style.flexWrap = "wrap";
 
-// id="mobile-tech"
+        techDiv.appendChild(techIconImg);
+        techDiv.appendChild(techName);
+        relatedDiv.appendChild(techDiv);
+    }
+});
