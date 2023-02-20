ddbdbaa765214cd432cea03271fe9b2bc8b8cd24
Muhammad Hamza <hamza.6.shakeel@gmail.com>
2023-02-20 16:38:55 +0500

    animate to top

--- js/animate.js
+++ js/animate.js
@@ -1,4 +1,12 @@
 $(document).ready(function () {
+    // For Scroll to Top Animation
+    $("#up").on("click", function () {
+        $("html, body").animate({
+            scrollTop: 0
+        }, 1500);
+        return false;
+    });
+
     // fade in's animation
     AOS.init({
         easing: "ease",
@@ -7,30 +15,30 @@ $(document).ready(function () {
     });
 });
 
-// $(document).ready(function () {
+$(document).ready(function () {
 
-//     window.onscroll = function () {
-//         $(document).on("scroll", onScroll);
+    window.onscroll = function () {
+        $(document).on("scroll", onScroll);
 
-//         //smooth scroll
-//         $('a[href^="#"]').on('click', function (e) {
-//             e.preventDefault();
-//             $(document).off("scroll");
+        //smooth scroll
+        $('a[href^="#"]').on('click', function (e) {
+            e.preventDefault();
+            $(document).off("scroll");
 
-//             $('a').each(function () {
-//                 $(this).removeClass('active');
-//             })
-//             $(this).addClass('active');
+            $('a').each(function () {
+                $(this).removeClass('active');
+            })
+            $(this).addClass('active');
 
-//             var target = this.hash,
-//                 menu = target;
-//             $target = $(target);
-//             $('html, body').stop().animate({
-//                 'scrollTop': $target.offset().top + 2
-//             }, 1000, 'swing', function () {
-//                 window.location.hash = target;
-//                 $(document).on("scroll", onScroll);
-//             });
-//         });
-//     }
-// });
+            var target = this.hash,
+                menu = target;
+            $target = $(target);
+            $('html, body').stop().animate({
+                'scrollTop': $target.offset().top + 2
+            }, 1000, 'swing', function () {
+                window.location.hash = target;
+                $(document).on("scroll", onScroll);
+            });
+        });
+    }
+});
