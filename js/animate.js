16089d9ce488386ebc4e99e228c3c605c61cf3db
Muhammad Hamza <hamza.6.shakeel@gmail.com>
2023-02-20 17:53:42 +0500

    scrolling fixed

--- js/animate.js
+++ js/animate.js
@@ -42,3 +42,18 @@ $(document).ready(function () {
         });
     }
 });
+
+function onScroll(event) {
+    var scrollPos = $(document).scrollTop();
+    $('#navbarMenu a').each(function () {
+        var currLink = $(this);
+        var refElement = $(currLink.attr("href"));
+        if (refElement.position().top <= scrollPos && refElement.position().top + refElement.height() > scrollPos) {
+            $('#navbarMenu ul li a').removeClass("active");
+            currLink.addClass("active");
+        }
+        else {
+            currLink.removeClass("active");
+        }
+    });
+}
