d196d11d645bacef26564228c4cec3dcbb875a7a
Muhammad Hamza <hamza.6.shakeel@gmail.com>
2023-02-22 11:02:32 +0500

    current date calander added

--- src/js/contact.js
+++ src/js/contact.js
@@ -1,4 +1,6 @@
 $(document).ready(function () {
+    currentDate();
+
     var contactInfo = [
         {
             "icon": "fa fa-phone",
@@ -48,4 +50,26 @@ $(document).ready(function () {
 
 function onMeetClick() {
     window.open("https://calendly.com/mhmzdev/15min", '_blank');
+}
+
+function currentDate() {
+    const current = new Date();
+    let monthName = current.toLocaleDateString('en-US', {
+        month: "long",
+    });
+    
+    let date = current.getDate();
+
+    let dayName = current.toLocaleDateString('en-US', {
+        weekday: "long",
+    });
+
+    var monthText = document.getElementById("month");
+    monthText.innerHTML = monthName;
+
+    var dayText = document.getElementById("date");
+    dayText.innerHTML = date;
+
+    var weekDayText = document.getElementById("day");
+    weekDayText.innerText = dayName;
 }
